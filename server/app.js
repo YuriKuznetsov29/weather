796ac25010c93d0d7c8596385e438d8a9a384978
Yuri Kuznetsov <playeer2905@gmail.com>
2023-06-26 23:31:32 +0300

    added nodemon and change chalk version

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ const app = express()
 
 const PORT = config.get('port') ?? 8080
 
+if (process.env.NODE_ENV = 'production') {
+
+}
+
 app.listen(PORT, () => {
     console.log(chalk.green(`Server has started on port`))
 })
