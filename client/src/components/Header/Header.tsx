807349a6fdd55b764d0f6eb97376173480c15121
Yuri Kuznetsov <playeer2905@gmail.com>
2023-07-07 19:24:54 +0300

    finished saving location function

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,80 +1,66 @@
-import { useContext, useEffect, useState, createContext } from 'react';
-import { List, Star } from '@phosphor-icons/react';
-import Container from 'components/Container/Container';
-import { useAppDispatch, useAppSelector } from 'app/hooks';
-import { currentCity, currentLocationSelector, userSelector } from 'app/selectors';
+import { useEffect, useState, createContext } from "react"
+import { List } from "@phosphor-icons/react"
+import Container from "components/Container/Container"
+import { useAppSelector } from "app/hooks"
+import { currentCity } from "app/selectors"
+import StarSvg from "./StarSvg"
 
-import { createSelector } from '@reduxjs/toolkit';
-
-import styles from './Header.module.scss';
-import { saveLocations } from 'app/slices/loginSlice';
-import { CurrentLocation } from 'app/slices/locationSlice';
+import styles from "./Header.module.scss"
 
 type BarContext = {
-    barState: boolean
-    stateChange: () => void
+  barState: boolean
+  stateChange: () => void
 }
 
 export const Context = createContext<BarContext>(null!)
 
 interface HeaderProps {
-    children: JSX.Element
-    background?: boolean
+  children: JSX.Element
+  background?: boolean
 }
 
-const Header = ({children, background}: HeaderProps) => {
-    const [scroll, setScroll] = useState({})
-    const [barState, setBarState] = useState(false)
+const Header = ({ children, background }: HeaderProps) => {
+  const [scroll, setScroll] = useState({})
+  const [barState, setBarState] = useState(false)
 
-    const dispatch = useAppDispatch()
-    const { userId, savedLocations } = useAppSelector(userSelector)
-    const currentLocation = useAppSelector(currentLocationSelector)
+  const stateChange = () => {
+    setBarState(!barState)
+  }
 
-    const stateChange = () => {
-        setBarState(!barState)
+  const city = useAppSelector(currentCity)
+
+  useEffect(() => {
+    document.body.onscroll = () => {
+      if (window.pageYOffset > 0) {
+        setScroll(styles.scroll)
+      } else {
+        setScroll({})
+      }
     }
 
-    const saveCurrentLocation = () => {
-        if (currentLocation) {
-            const newLocations: CurrentLocation[] = savedLocations.concat([currentLocation])
-            dispatch(saveLocations({userId, savedLocations: newLocations}))
-        }
-        
+    return () => {
+      document.body.onscroll = null
     }
+  })
 
-    const city = useAppSelector(currentCity)
-
-    useEffect(() => {
-        document.body.onscroll = () => {
-            if (window.pageYOffset > 0) {
-                setScroll(styles.scroll)
-            } else {
-                setScroll({})
-            }
-        }
-
-        return () => {
-            document.body.onscroll = null
-        }
-    })
-
-    return (
-        <Context.Provider value={{barState, stateChange}}>
-        <header className={`${styles.header} ${scroll} ${background && styles.scroll}`}>
-            <Container>
-                <div className={styles.header__inner}>
-                    <div>
-                        <div className={styles.location__city} data-type="city">{city}</div>
-                        <Star size={24} color="#deb25c" weight="bold" onClick={saveCurrentLocation} />
-                    </div>
-
-                    <List className={styles.burger} onClick={() => stateChange()} />
-                </div>
-            </Container>
-        </header>
-        {children}
-        </Context.Provider>
-    )
+  return (
+    <Context.Provider value={{ barState, stateChange }}>
+      <header className={`${styles.header} ${scroll} ${background && styles.scroll}`}>
+        <Container>
+          <div className={styles.header__inner}>
+            <div className={styles.city_wrapper}>
+              <div className={styles.location__city} data-type="city">
+                {city}
+              </div>
+              <StarSvg />
+            </div>
+            <List className={styles.burger} onClick={() => stateChange()} />
+          </div>
+        </Container>
+      </header>
+      {children}
+    </Context.Provider>
+  )
 }
 
 export default Header
---
+++ client/src/components/Header/StarSvg.tsx
@@ -0,0 +1,52 @@
+import { userSelector, currentLocationSelector } from "app/selectors"
+import { useAppSelector, useAppDispatch } from "app/hooks"
+import { CurrentLocation } from "app/slices/locationSlice"
+import { saveLocations } from "app/slices/loginSlice"
+
+import styles from "./Header.module.scss"
+
+const StarSvg = () => {
+    const dispatch = useAppDispatch()
+    const currentLocation = useAppSelector(currentLocationSelector)
+    const { userId, savedLocations } = useAppSelector(userSelector)
+
+    const checkInclude = () => {
+        if (currentLocation && savedLocations) {
+            return Boolean(
+                savedLocations.find((location) => location.city === currentLocation.city)
+            )
+        }
+        return false
+    }
+
+    const saveCurrentLocation = () => {
+        if (currentLocation) {
+            const newLocations: CurrentLocation[] = checkInclude()
+                ? savedLocations.filter((location) => location.city !== currentLocation.city)
+                : savedLocations.concat([currentLocation])
+            dispatch(saveLocations({ userId, savedLocations: newLocations }))
+        }
+    }
+
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 256 256"
+            width="30"
+            height="30"
+            onClick={saveCurrentLocation}
+        >
+            <rect width="256" height="256" fill="none" />
+            <path
+                className={`${styles.star} ${checkInclude() && styles.activeStar}`}
+                d="M135.34,28.9l23.23,55.36a8,8,0,0,0,6.67,4.88l59.46,5.14a8,8,0,0,1,4.54,14.07L184.13,147.7a8.08,8.08,0,0,0-2.54,7.89l13.52,58.54a8,8,0,0,1-11.89,8.69l-51.1-31a7.93,7.93,0,0,0-8.24,0l-51.1,31a8,8,0,0,1-11.89-8.69l13.52-58.54a8.08,8.08,0,0,0-2.54-7.89L26.76,108.35A8,8,0,0,1,31.3,94.28l59.46-5.14a8,8,0,0,0,6.67-4.88L120.66,28.9A8,8,0,0,1,135.34,28.9Z"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="16"
+            />
+        </svg>
+    )
+}
+
+export default StarSvg
