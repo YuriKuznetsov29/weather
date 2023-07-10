bc30d36b0595a69e21342ba67821c91702fd7082
Yuri <player2905@gmail.com>
2023-07-10 16:48:57 +0300

    fix bug

--- client/src/components/Header/StarSvg.tsx
+++ client/src/components/Header/StarSvg.tsx
@@ -29,7 +29,7 @@ const StarSvg = () => {
         if (currentLocation && auth) {
             const newLocations: CurrentLocation[] = checkInclude()
                 ? savedLocations.filter((location) => location.city !== currentLocation.city)
-                : savedLocations.concat([currentLocation])
+                : saveLocations.length > 0 ? savedLocations.concat([currentLocation]) : [currentLocation]
             dispatch(saveLocations({ userId, savedLocations: newLocations }))
         } else {
             navigate('/signIn')
