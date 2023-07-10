19fd1163f11f8f021ad6fedbbabec6466a533bdb
Yuri <player2905@gmail.com>
2023-07-10 16:48:57 +0300

    fix bug

--- client/src/components/http/index.ts
+++ client/src/components/http/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { AuthResponse } from "components/models/response/AuthResponse";
 
-export const API_URL = `http://localhost:8080/api`
+export const API_URL = `http://yura.batgen.ru:8080/api`
 
 //84.38.183.52
 
